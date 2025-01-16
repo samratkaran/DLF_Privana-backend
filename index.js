1ca82f8979c7fc3a2e8b83075224f4254ba76858
samratkaran <karansamrat05039@gmail.com>
2025-01-16 15:53:09 +0530

    email chnaged

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/form', (req, res) => {
 
     const mailOptions = {
         from: 'karan@ebaseinfra.com',  // Sender email
-        to: 'info@ebaseinfra.com',     // Recipient email
+        to: 'ebaseinfratech@gmail.com',     // Recipient email
         subject: 'DLF Privana North Enquiry',
         html:`<table width='60%' style='border:1px solid black;'>
                   <th style='background-color:#0051A4;color:#FFFFFF;text-align: center;'>Description</th>
